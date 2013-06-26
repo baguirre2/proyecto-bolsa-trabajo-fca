e7721495e1df43ff8e4de7c6b7587c630d78b6d7
sdarik7@gmail.com <sdarik7@gmail.com@2194bc8b-58a6-bfba-a5b4-b41c2c81ddfe>
2013-06-26 14:39:43 +0000

    Creación entidad Alumno

---
+++ trunk/SIBT/entities/Alumno.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * Archivo: Class Alumno
+ * Autor:	Emmanuel García C.
+ * Fecha:	Martes 25/Junio/2013
+ * Modificaciones: 
+ * -
+ * -
+ */
+
+class Alumno{
+
+	function __construct() {
+	}
+	
+	
+}
+
+?>
