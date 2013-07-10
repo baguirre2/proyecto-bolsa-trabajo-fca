90a61ced48f178570647cc7c24440b56d4af85d3
sdarik7@gmail.com <sdarik7@gmail.com@2194bc8b-58a6-bfba-a5b4-b41c2c81ddfe>
2013-07-10 01:29:21 +0000

    Archivo para carga de imagenes al servidor

---
+++ trunk/SIBT/entities/upload.php
@@ -0,0 +1,37 @@
+<?php 
+
+/*
+ * Archivo: Archivo upload.php
+ * Autor:	Emmanuel García C.
+ * Fecha:	Martes 25/Junio/2013
+ * Modificaciones: 
+ * -
+ * -
+ */
+
+$tipo = $dir;
+
+$upload_folder ='./../constancias/'.$tipo;
+//var_dump($_FILES);
+
+//$return = Array('ok'=>TRUE, 'msg' => "Imagen subida exitosamente");
+$return = "Imagen subida exitosamente";
+
+$nombre_archivo = $_FILES['archivo']['name'];
+$tipo_archivo = $_FILES['archivo']['type'];
+$tamano_archivo = $_FILES['archivo']['size'];
+$tmp_archivo = $_FILES['archivo']['tmp_name'];
+$archivador = $upload_folder . '/' . $nombre_archivo;
+ 
+if (!move_uploaded_file($tmp_archivo, $archivador)) {
+	//$return = Array('ok' => FALSE, 'msg' => "Ocurrio un error al subir la imagen.", 'status' => 'error');
+	$return = false;
+	echo json_encode($return);
+	return json_encode(false);
+}else{
+	$return = $nombre_archivo;
+	echo $return;
+	return $nombre_archivo;
+}
+
+?>
