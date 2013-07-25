6f2fd85d5b4f38812aa1955ce8db97dc8a769460
aljojojo3 <aljojojo3@2194bc8b-58a6-bfba-a5b4-b41c2c81ddfe>
2013-07-25 02:29:09 +0000

    Modificación en la estructura del formulario y la manera de mostrar los datos de estado y delegación/municipio.

--- SIBT/boundaries/alumno/frmModDireccion.php
+++ SIBT/boundaries/alumno/frmModDireccion.php
@@ -1,92 +1,75 @@
 <?php
 class FrmMiDireccion{
-	
-	function __construct($catalogos,$datos_dir){
+    
+    function __construct($catalogos,$datos_dir, $alumno_id){
 ?>
-<div id="res_mi_direccion"></div>
-<form id="mi_direccion" action="#">
-	<input type="hidden" value="" name="opc" />
-	<input type="hidden" value="<?php echo $datos_dir['do_id'];?>" name="direccion[do_id]" /><!-- Opción del Controller -->
-	<input type="hidden" value="<?php echo $id_alumno ?>" name="direccion[id_alumno]" />
-	
-	<fieldset>
-		<table>
-			<tr>
-				<td>C.P.</td>
-				<td><input type="text" name="direccion[co_codigo_postal]" id="co_codigo_postal" value="<?php echo $datos_dir['co_codigo_postal']?>" required="numeric"/></td>
-			</tr>
-			<tr>
-				<th>Actualizar mi dirección</th>
-			</tr>
-			<tr>
-				<td>Entidad Federativa:</td>
-				<td ><select name="direccion[es_id]" id="es_id" onchange="obtenerDeMu();return false;">
-						<?php
-							foreach($catalogos['estados'] as $clave=> $valor ){
-								if($datos_dir['es_id'] == $clave){
-									echo "<option value='".$clave."' selected>".$valor."</option>\n";
-								}else{
-									echo "<option value='".$clave."'>".$valor."</option>\n";
-								}
-							}
-						?>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td>Delegación/Municipio:</td>
-				<td id="td_demu_id"><select name="direccion[demu_id]" id="demu_id" onchange="obtenerColonia();return false;">
-						<?php
-							foreach($catalogos['delegaciones'] as $clave=> $valor ){
-								if($datos_dir['demu_id'] == $clave){
-									echo "<option value='".$clave."' selected>".$valor."</option>\n";
-								}else{
-									echo "<option value='".$clave."'>".$valor."</option>\n";
-								}
-							}
-						?>
-					</select>				
-				</td>
-			</tr>
-			<tr>
-				<td>Colonia:</td>
-				<td id="td_co_id"><select name="direccion[co_id]" id="co_id">
-						<?php
-							foreach($catalogos['colonias'] as $clave=> $valor ){
-								if($datos_dir['co_id'] == $clave){
-									echo "<option value='".$clave."' selected>".$valor."</option>\n";
-								}else{
-									echo "<option value='".$clave."'>".$valor."</option>\n";
-								}
-							}
-						?>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td>Calle</td>
-				<td><input type="text" name="direccion[do_calle]" id="do_calle" value="<?php echo $datos_dir['do_calle']?>"/></td>
-			</tr>
-			<tr>
-				<td>Número Ext.:</td>
-				<td><input type="text" name="direccion[do_num_exterior]" id="do_num_exterior" value="<?php echo $datos_dir['do_num_exterior']?>" required="numeric"/></td>
-			</tr>
-			<tr>
-				<td>Número Int.:</td>
-				<td><input type="text" name="direccion[do_num_interior]" id="do_num_interior" value="<?php echo $datos_dir['do_num_interior']?>" required="numeric"/></td>
-			</tr>
-		</table>
-		
-		<table>
-			<tr>
-				<td><input type="button" value="Registrar" onclick="ajax('controllers/gestionarAlumno/CtlAlumno.php','actMiDireccion','mi_direccion','res_mi_direccion');"/></td>
-				<td><input type="button" value="Regresar" onclick="ajax('controllers/gestionarAlumno/CtrlAlumno.php','actAlumno','vacio','contenido');"/></td>
-			</tr>
-		</table>
-	</fieldset>
+<form id="frmMiDireccion">
+    <input type="hidden" value="<?php echo $datos_dir['do_id'];?>" name="do_id" id="do_id" /><!-- Opciï¿½n del Controller -->
+    <input type="hidden" value="<?php echo $alumno_id ?>" name="id" id="id" /><!-- ID del alumno-->
+
+        <table>
+            <tr>
+                <th>Actualizar mi direcci&oacuten</th>
+            </tr>
+            <tr>
+                <td>C.P.</td>
+                <td><input type="text" name="co_codigo_postal" id="co_codigo_postal" value="<?php echo $datos_dir['co_codigo_postal']?>" class="required" maxlength="5" onchange="obtenerDatosCp()"/></td>
+            </tr>
+            <tr>
+                <td>Calle</td>
+                <td><input type="text" name="do_calle" id="do_calle" value="<?php echo $datos_dir['do_calle']?>" class="required" /></td>
+            </tr>
+            <tr>
+                <td>N&uacutemero Ext.:</td>
+                <td><input type="text" name="do_num_exterior" id="do_num_exterior" value="<?php echo $datos_dir['do_num_exterior']?>" class="required"/></td>
+            </tr>
+            <tr>
+                <td>N&uacutemero Int.:</td>
+                <td><input type="text" name="do_num_interior" id="do_num_interior" value="<?php echo $datos_dir['do_num_interior']?>"/></td>
+            </tr>
+        </table>
+        
+        <table class="tab_form" id="datos_cp">
+	        <tr>
+                <td>Entidad Federativa:</td>
+                <td><input type="text" name="es_nombre" id="direccion[es_nombre]" value="<?php echo $datos_dir['es_nombre']; ?>" readonly />
+                	<input type="hidden" name="es_id" id="es_id" value="<?php echo $datos_dir['es_id']?>" />
+                </td>
+            </tr>
+            <tr>
+                <td>Delegaci&oacuten/Municipio:</td>
+                <td><input type="text" name="demu_nombre" id="demu_nombre" value="<?php echo $datos_dir['demu_nombre'];?>" readonly />
+                	<input type="hidden" name="demu_id" id="demu_id" value="<?php echo $datos_dir['demu_id']?>" />
+                </td>
+            </tr>
+            <tr>
+                <td>Colonia:</td>
+                <td><select name="co_id" id="co_id">
+                        <?php
+                            foreach($catalogos['colonias'] as $clave=> $valor ){
+                                if($datos_dir['co_id'] == $valor['co_id']){
+                                    echo "<option value='".$valor['co_id']."' selected>".$valor['co_nombre']."</option>\n";
+                                }else{
+                                    echo "<option value='".$valor['co_id']."'>".$valor['co_nombre']."</option>\n";
+                                }
+                            }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            
+        </table>
+        
+        <table>
+            <tr>
+                <td><input type="button" value="Registrar" onclick="ajax('./controllers/gestionarAlumno/CtlAlumno.php','actMiDireccion','frmMiDireccion','contenido');"/></td>
+                <td><input type="button" value="Regresar" onclick="ajax('./controllers/gestionarAlumno/CtlAlumno.php','actAlumno','vacio','contenido');"/></td>
+            </tr>
+        </table>
+
 </form>
-<script src="webroot/js/frmMiDirAlumno.js"></script>
-<?php	
-	}
+<script src="./webroot/js/frmMiDirAlumno.js"></script>
+<?php   
+    }
 }
 ?>
